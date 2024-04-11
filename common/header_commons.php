d296c575a589cab0f04abcb6199d5ed5df5c1965
Sankalpana Karunarathna <sankalpana123@gmail.com>
2024-04-11 15:39:52 +0530

    layout improvements

---
+++ common/header_commons.php
@@ -0,0 +1,9 @@
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="Author">
+        <link rel="stylesheet" href="./css/styles.css">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Dosis:wght@200..800&display=swap');
+        </style>
