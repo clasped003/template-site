28a0768683afba0a3cfaea2bdc6be5ee3f404baa
clasped003 <84282381+clasped003@users.noreply.github.com>
2024-04-14 23:17:53 -0700

    Update about.php
    Added Profile pics and updated bios for demonstration purposes.
    Added border effect to outline of the pics.

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
         <meta name="description" content="">
         <meta name="keywords" content="">
         <meta name="author" content="Author">
-        <link rel="stylesheet" href="./css/styles.css">
+        <link rel="stylesheet" href="./css/styles.css?v=<?php echo time(); ?>">
         <style>
             <!-- Custom Font URL -->
             @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Dosis:wght@200..800&display=swap')
@@ -40,15 +40,19 @@
             <h2>About Body Part</h2>
             <div class="aboutBoxContainer">
                 <div class="aboutBox">
-                    <!--<img src="./img/headhsotpic.png" alt="#">-->
+                    <div class="staffPhoto1">
+                        <!--<img src="./img/headhsotpic.png" alt="#">-->
+                    </div>
+                    <br>
+                    <br>
                     <h3>Business Owner, MBA</h3>
                     <p>
                         Hello, my name is Owner, and I am the founder of This Business. <br>
-                        I have a Masters Degree in Business from Local University, as well as
+                        I have a Masters Degree in Business from Local University,<br> as well as
                         a Masters in Business Administration from the University of the Locals (Go Team!). <br>
                         In addition, I am a Brand Pro certified specialist.<br>
-                        I have spent over half a decade working for local businesses<br>
-                        in the area. I am now branching out to offer private business services
+                        I have spent over half a decade working for local businesses
+                        in the area.<br> I am now branching out to offer private business services
                         to other businesses in the Local area and beyond.<br>                       
                     </p>
                     <p>
@@ -58,7 +62,25 @@
                     </p>
                 </div>
             </div>
+            <div class="aboutBoxContainer">
+                <div class="aboutBox">
+                    <div class="staffPhoto2">
+                    <!--<img src="./img/headhsotpic.png" alt="#">-->
+                    </div>
+                    <br>
+                    <br>
+                    <h3>Staff Member</h3>
+                    <p>Hello there, my name is Staff Member. I have several years of experience in this industry.<br>
+                            I have previous experience with business work, and accounting work.<br>
+                    </p>
+                    <p>When I am not in office assisting clients,<br> you can catch me exploring the
+                    the great outdoors in my local vicinity.  
+                    </p>
+                </div>
+            </div>
         </div>
+        <br>
+        <br>
         <div class="Footer">
             <!--Below element for testing only-->
             <h2>About Footer Part</h2>
